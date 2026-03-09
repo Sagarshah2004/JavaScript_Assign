7325afb9fdb6dba524ac76b9fb6a1265e3c1aec5
Sagarshah2004 <sagar1322sha@gmail.com>
2026-03-09 21:25:26 +0530

    INITIAL COMMIT

---
+++ Task1.js
@@ -0,0 +1,20 @@
+
+
+const arr = [23, 45, 67, 89, 12, 90, 44];
+
+let largest = -Infinity;
+let secondLargest = -Infinity;
+
+for (let num of arr) {
+  if (num > largest) {
+    secondLargest = largest;
+    largest = num;
+  } 
+  else if (num > secondLargest && num !== largest) {
+    secondLargest = num;
+  }
+}
+
+console.log(secondLargest);
+
+
